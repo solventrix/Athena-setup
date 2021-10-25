ddf4ff5385ef876b3b23dd03e9407713a8322ada
petermoorthamer <pmoorth1@its.jnj.com>
2021-10-25 08:28:51 +0200

    Add docker login command

--- RunDQD/runDQD.sh
+++ RunDQD/runDQD.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
+echo "Harbor image repo login"
+docker login harbor-uat.athenafederation.org
+
 touch dqd.env
 
 echo "THERAPEUTIC_AREA=ATHENA" >> dqd.env
 echo "SCRIPT_UUID=2e72f604-62da-4e1e-8523-8495103586be"  >> dqd.env
 
+echo "Run DQD..."
 docker run \
 --rm \
 --name dqd \
