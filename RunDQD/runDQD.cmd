0649ea2860d39537646dc078f47bf4d25cd6cf52
Peter Moorthamer <moorthamer@gmail.com>
2023-07-11 16:50:35 +0200

    Use latest version of Data Quality Dashboard (DQD)

--- RunDQD/runDQD.cmd
+++ RunDQD/runDQD.cmd
@@ -1,13 +1,7 @@
 @echo off
 
-echo "THERAPEUTIC_AREA=ATHENA" > dqd.env
-echo "SCRIPT_UUID=2e72f604-62da-4e1e-8523-8495103586be"  >> dqd.env
+docker login harbor.athenafederation.org
 
-docker run \
---rm \
---name dqd \
---env-file dqd.env \
---network athena-net \
-harbor-uat.athenafederation.org/distributed-analytics/data-quality-dashboard:2.0.14
+docker pull harbor.athenafederation.org/distributed-analytics/data-quality-dashboard:latest
 
-Del dqd.env
+docker run --rm --name dqd --env THERAPEUTIC_AREA=ATHENA -v %CD%\output:/script/output --network feder8-net harbor.athenafederation.org/distributed-analytics/data-quality-dashboard:latest
--- RunDQD/runDQD.sh
+++ RunDQD/runDQD.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 set -ex
 
+REGISTRY=harbor.athenafederation.org
+REPOSITORY=distributed-analytics
+IMAGE=data-quality-dashboard
+VERSION=latest
+TAG=$VERSION
+
 echo "Harbor image repo login"
-docker login harbor-uat.athenafederation.org
+docker login $REGISTRY
+
+echo "Pull latest DQD image"
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 touch dqd.env
 
 echo "THERAPEUTIC_AREA=ATHENA" >> dqd.env
-echo "SCRIPT_UUID=2e72f604-62da-4e1e-8523-8495103586be"  >> dqd.env
 
 echo "Run DQD..."
 docker run \
 --rm \
 --name dqd \
 --env-file dqd.env \
---network athena-net \
-harbor-uat.athenafederation.org/distributed-analytics/data-quality-dashboard:2.0.14
+-v ${PWD}/output:/script/output \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 rm -rf dqd.env
