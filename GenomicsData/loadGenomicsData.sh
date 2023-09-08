c3579916ffb838f29f61411af31e13811e941eda
Peter Moorthamer <moorthamer@gmail.com>
2023-09-08 15:17:26 +0200

    New script to load genomics test data for ATHENA UC (JIRA AEKR-1921)

--- GenomicsData/loadGenomicsData.sh
+++ GenomicsData/loadGenomicsData.sh
@@ -14,3 +14,5 @@ docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 docker exec -it postgres psql -U postgres -d OHDSI -c "ALTER TABLE $CDM_SCHEMA.measurement ALTER measurement_source_value TYPE VARCHAR(4096);"
 
 docker run --rm --network feder8-net --env DB_CDM_SCHEMA=$CDM_SCHEMA --env THERAPEUTIC_AREA=ATHENA $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "end of script"
