bca10f76e7591d3802e59351ac8ee384c023b337
Peter Moorthamer <moorthamer@gmail.com>
2023-10-24 16:53:47 +0200

    Adapt script to load real genomics data @ UZL UC (JIRA AEKR-1951)

--- GenomicsData/loadGenomicsData.sh
+++ GenomicsData/loadGenomicsData.sh
@@ -3,16 +3,23 @@ set -eux
 
 REGISTRY=harbor.athenafederation.org
 REPOSITORY=distributed-analytics
-IMAGE=genetic-data-loader
-VERSION=1.0.0
+IMAGE=genomics-data-loader
+VERSION=1.1.0
 TAG=$VERSION
+THERAPEUTIC_AREA=ATHENA
 CDM_SCHEMA=omopcdm
+
+read -p "Input Data folder [./data]: " DATA_FOLDER
+DATA_FOLDER=${DATA_FOLDER:-${PWD}/data}
 
 docker login $REGISTRY
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
+echo "Delete old test data"
+docker exec -it postgres psql -U postgres -d OHDSI -c "DELETE FROM $CDM_SCHEMA.measurement WHERE measurement_date = '2023-09-07';"
+echo "Increase length of 'measurement_source_value' column"
 docker exec -it postgres psql -U postgres -d OHDSI -c "ALTER TABLE $CDM_SCHEMA.measurement ALTER measurement_source_value TYPE VARCHAR(4096);"
-
-docker run --rm --network feder8-net --env DB_CDM_SCHEMA=$CDM_SCHEMA --env THERAPEUTIC_AREA=ATHENA $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+echo "Load genomics data"
+docker run --rm --network feder8-net -v "$DATA_FOLDER":/script/data --env DB_CDM_SCHEMA=$CDM_SCHEMA --env THERAPEUTIC_AREA=$THERAPEUTIC_AREA $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 echo "end of script"
