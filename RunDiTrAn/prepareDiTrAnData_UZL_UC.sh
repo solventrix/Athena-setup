447a9a4c44f6bcbcade34e4f2688eb03dd725850
Peter Moorthamer <moorthamer@gmail.com>
2024-01-24 10:18:00 +0100

    Update script for hr study (ATHENA UZL UC)

--- RunDiTrAn/prepareDiTrAnData_UZL_UC.sh
+++ RunDiTrAn/prepareDiTrAnData_UZL_UC.sh
@@ -1,38 +1,21 @@
 docker login harbor.athenafederation.org
 
-docker pull harbor.athenafederation.org/distributed-analytics/event-generator:0.2.5
-docker pull harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.4
+docker pull harbor.athenafederation.org/distributed-analytics/event-generator:0.2.6
+docker pull harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.5
 docker pull harbor.athenafederation.org/athena-restricted/ditran-data-pipeline-uc:1.5.2
 
-mkdir -p ${PWD}/results/predefined_events
-mkdir -p ${PWD}/results/derived_events
-mkdir -p ${PWD}/results/predefined_analysis
-mkdir -p ${PWD}/results/derived_analysis
-mkdir -p ${PWD}/results/hybrid_analysis
+mkdir -p ${PWD}/results/hr_events
+mkdir -p ${PWD}/results/hr_analysis
 mkdir -p ${PWD}/results/ditran
 
-echo "run 'predefined' event generator"
-docker run --rm --network feder8-net -v ${PWD}/results/predefined_events:/script/analysis_table_results --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env EVENT_CONFIG_FILENAME=uc_event_model --env STUDY_LOGIC=uzl_uc --env EPISODE_TABLE_SCHEMA=results --env LOGGING=INFO --env EPISODE_TABLE_NAME=predefined_episode_table_uc harbor.athenafederation.org/distributed-analytics/event-generator:0.2.5
+echo "run event generator"
+docker run --rm --network feder8-net -v ${PWD}/results/hr_events:/script/analysis_table_results --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env STUDY_LOGIC=uzl_uc_hr_study --env EPISODE_TABLE_SCHEMA=results --env EPISODE_TABLE_NAME=episode_table_uc_hr --env LOGGING=INFO harbor.athenafederation.org/distributed-analytics/event-generator:0.2.6
 
-echo "run 'derived' event generator"
-docker run --rm --network feder8-net -v ${PWD}/results/derived_events:/script/analysis_table_results --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env STUDY_LOGIC=uzl_uc --env EPISODE_TABLE_SCHEMA=results --env LOGGING=INFO --env EPISODE_TABLE_NAME=derived_episode_table_uc harbor.athenafederation.org/distributed-analytics/event-generator:0.2.5
+echo "create analysis table"
+docker run --rm --network feder8-net -v ${PWD}/results/hr_analysis:/script/results --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env VERSION=1.1.5 --env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=analysis_table_uc --env EXPOSURE_TABLE_SCRIPT=exposure_uc_omop53 --env DERIVATE_TABLE_SCRIPT=derivate_uc --env OUTCOME_TABLE_SCRIPT=outcome_uc --env EPISODE_TABLE_SCHEMA=results --env EPISODE_TABLE_NAME=episode_table_uc_hr --env COVARIATE_CONFIGURATION=configuration_uc harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.5
 
-echo "create table 'results.hybrid_episode_table_uc'"
-docker exec -it postgres psql -U athena_admin -d OHDSI -c "DROP TABLE IF EXISTS results.hybrid_episode_table_uc;CREATE TABLE results.hybrid_episode_table_uc as select * from ( select * from results.derived_episode_table_uc where episode_number = 0 union select * from results.predefined_episode_table_uc where episode_number > 0) hybrid_table where person_id in ( select person_id from results.derived_episode_table_uc where episode_number = 0 );"
-
-echo "create analysis table with predefined events"
-docker run --rm --network feder8-net -v ${PWD}/results/predefined_analysis:/script/results --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env VERSION=1.1.4 --env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=predefined_analysis_table_uc --env EXPOSURE_TABLE_SCRIPT=exposure_uc_omop53 --env DERIVATE_TABLE_SCRIPT=derivate_uc --env OUTCOME_TABLE_SCRIPT=outcome_uc --env EPISODE_TABLE_SCHEMA=results --env EPISODE_TABLE_NAME=predefined_episode_table_uc --env COVARIATE_CONFIGURATION=configuration_uc harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.4
-
-echo "create analysis table with derived events"
-docker run --rm --network feder8-net -v ${PWD}/results/derived_analysis:/script/results  --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env VERSION=1.1.4 --env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=derived_analysis_table_uc --env EXPOSURE_TABLE_SCRIPT=exposure_uc_omop53 --env DERIVATE_TABLE_SCRIPT=derivate_uc --env OUTCOME_TABLE_SCRIPT=outcome_uc --env EPISODE_TABLE_SCHEMA=results --env EPISODE_TABLE_NAME=derived_episode_table_uc --env COVARIATE_CONFIGURATION=configuration_uc harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.4
-
-echo "create analysis table with hybrid events"
-docker run --rm --network feder8-net -v ${PWD}/results/hybrid_analysis:/script/results  --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env VERSION=1.1.4 --env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=hybrid_analysis_table_uc --env EXPOSURE_TABLE_SCRIPT=exposure_uc_omop53 --env DERIVATE_TABLE_SCRIPT=derivate_uc --env OUTCOME_TABLE_SCRIPT=outcome_uc --env EPISODE_TABLE_SCHEMA=results --env EPISODE_TABLE_NAME=hybrid_episode_table_uc --env COVARIATE_CONFIGURATION=configuration_uc --env COMPUTE_NEXT_EPISODE='TRUE' harbor.athenafederation.org/distributed-analytics/analysis-table-generator-uc:1.1.4
-
-echo "combine analysis tables"
-docker exec -it postgres psql -U athena_admin -d OHDSI -c "DROP TABLE IF EXISTS results.combined_analysis_table_uc;CREATE TABLE results.combined_analysis_table_uc as select *, 'predefined' as event_type from results.predefined_analysis_table_uc union select *, 'derived' as event_type from results.derived_analysis_table_uc union select *, 'hybrid' as event_type from results.hybrid_analysis_table_uc;"
-echo "correct DB permissions if needed"
-docker exec -it postgres psql -U postgres -d OHDSI -c "REASSIGN OWNED BY athena_admin TO ohdsi_admin;REASSIGN OWNED BY ohdsi_app_user TO ohdsi_app;grant select on results.combined_analysis_table_uc to ohdsi_app;"
+echo "Set DB permissions"
+docker exec -it postgres psql -U postgres -d OHDSI -c "REASSIGN OWNED BY athena_admin TO ohdsi_admin;grant select on results.analysis_table_uc to ohdsi_app;"
 
 echo "run DiTrAn pipeline script"
 docker run --rm --network feder8-net -v ${PWD}/results/ditran:/script/shareable_results --name disease-explorer-data-preparation -v disease-explorer-config:/script/data --env THERAPEUTIC_AREA=athena --env INDICATION=uc --env DB_ANALYSIS_TABLE_SCHEMA=results --env DB_ANALYSIS_TABLE_NAME=combined_analysis_table_uc --env PIPELINE_CONFIGURATION=uc --env CONFIG_FILENAME=journey_configuration_combined --env LOGGING=INFO harbor.athenafederation.org/athena-restricted/ditran-data-pipeline-uc:1.5.2
