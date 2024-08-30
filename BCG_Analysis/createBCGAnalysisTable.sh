2ca5162453272426157a6aa218d1b455a13a2e81
Peter Moorthamer <moorthamer@gmail.com>
2024-08-30 16:30:25 +0200

    Update script version and volume mount for the query results (JIRA AEKR-2094)

--- BCG_Analysis/createBCGAnalysisTable.sh
+++ BCG_Analysis/createBCGAnalysisTable.sh
@@ -4,7 +4,7 @@ set -eux
 REGISTRY=harbor.athenafederation.org
 REPOSITORY=distributed-analytics
 IMAGE=bcg-analysis-table
-VERSION=1.0.0
+VERSION=1.0.1
 TAG=$VERSION
 
 echo "Log into $REGISTRY"
@@ -17,6 +17,7 @@ echo "Running Docker container..."
 docker run --rm \
 --name bcg-analysis-table \
 --env THERAPEUTIC_AREA=athena \
+-v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
