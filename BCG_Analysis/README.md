c82bd8aab7e93a555bd824617c246cde2189087b
Peter Moorthamer <moorthamer@gmail.com>
2024-09-09 15:56:30 +0200

    Add shell script to add BCG variables to DiTrAn (JIRA AEKR-2103)

--- BCG_Analysis/README.md
+++ BCG_Analysis/README.md
@@ -1,7 +1,25 @@
+# Extend analysis table and DiTrAn with BCG data for ATHENA UC
+
+## Context
+The script will create an analysis table for BCG treatments in the results schema of the local OMOP database,
+extend the existing analysis table (results.analysis_table_uc) and prepare the data for DiTrAn
+
+## Running the script
+To add BCG data to DiTrAn, please download and run the following script.
+###  1. Download the script
+```
+curl -fsSL https://raw.githubusercontent.com/solventrix/Athena-Setup/master/BCG_Analysis/updateDiTrAnWithBCG.sh --output updateDiTrAnWithBCG.sh && chmod +x updateDiTrAnWithBCG.sh
+```
+###  2. Run the script
+```
+./updateDiTrAnWithBCG.sh
+```
+
+
 # Create BCG analysis table for ATHENA UC
 
 ## Context
-The script will create an analysis table for BCG treatments in the results schema of the local OMOP daatabase
+The script will create an analysis table for BCG treatments in the results schema of the local OMOP database
 
 ## Running the script
 To create the analysis table, please download and run the following script.
---
+++ BCG_Analysis/updateDiTrAnWithBCG.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -eux
+
+REGISTRY=harbor.athenafederation.org
+
+echo "Log into $REGISTRY"
+docker login $REGISTRY
+
+echo "Running BCG analysis..."
+REPOSITORY=distributed-analytics
+IMAGE=bcg-analysis-table
+VERSION=1.0.8
+TAG=$VERSION
+
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+mkdir -p ${PWD}/results/bcg
+
+docker run --rm \
+--name bcg-analysis-table \
+--env THERAPEUTIC_AREA=athena \
+-v $PWD/results/bcg:/script/results \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "run DiTrAn data pipeline"
+REPOSITORY=athena-restricted
+IMAGE=ditran-data-pipeline-uc
+VERSION=1.5.5
+TAG=$VERSION
+
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+mkdir -p ${PWD}/results/ditran
+
+docker run --rm \
+--name ditran-data-preparation \
+--env THERAPEUTIC_AREA=athena --env INDICATION=uc --env DB_ANALYSIS_TABLE_SCHEMA=results --env DB_ANALYSIS_TABLE_NAME=analysis_table_uc --env PIPELINE_CONFIGURATION=uc --env CONFIG_FILENAME=journey_configuration --env LOGGING=INFO \
+-v ${PWD}/results/ditran:/script/shareable_results  \
+-v disease-explorer-config:/pipeline/data \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "end of script"
+
