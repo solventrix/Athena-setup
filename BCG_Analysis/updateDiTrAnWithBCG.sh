67231e502ddea73491082da029a14ccf63833bc1
Peter Moorthamer <moorthamer@gmail.com>
2024-09-11 17:30:59 +0200

    Updated analysis table for ATHENA UC (JIRA AEKR-2103)

--- BCG_Analysis/updateDiTrAnWithBCG.sh
+++ BCG_Analysis/updateDiTrAnWithBCG.sh
@@ -6,10 +6,31 @@ REGISTRY=harbor.athenafederation.org
 echo "Log into $REGISTRY"
 docker login $REGISTRY
 
+echo "Creating analysis table"
+REPOSITORY=distributed-analytics
+IMAGE=analysis-table-generator-uc
+VERSION=1.1.11
+TAG=$VERSION
+
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+mkdir -p ${PWD}/results/analysis
+
+docker run --rm \
+--network feder8-net -v ${PWD}/results/analysis:/script/results \
+--env THERAPEUTIC_AREA=athena --env INDICATION=uc --env VERSION=$VERSION \
+--env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=analysis_table_uc \
+--env EXPOSURE_TABLE_SCRIPT= --env EXPOSURE_TABLE_NAME= \
+--env DERIVATE_TABLE_SCRIPT=derivate_uc \
+--env OUTCOME_TABLE_SCRIPT=outcome_uc --env EPISODE_TABLE_SCRIPT=episode_uc \
+--env COVARIATE_CONFIGURATION=configuration_uc_hr \
+--env ADD_RETRO_COLUMNS=FALSE \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
 echo "Running BCG analysis..."
 REPOSITORY=distributed-analytics
 IMAGE=bcg-analysis-table
-VERSION=1.0.9
+VERSION=1.0.10
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
@@ -35,7 +56,9 @@ mkdir -p ${PWD}/results/ditran
 
 docker run --rm \
 --name ditran-data-preparation \
---env THERAPEUTIC_AREA=athena --env INDICATION=uc --env DB_ANALYSIS_TABLE_SCHEMA=results --env DB_ANALYSIS_TABLE_NAME=analysis_table_uc --env PIPELINE_CONFIGURATION=uc --env CONFIG_FILENAME=journey_configuration --env LOGGING=INFO \
+--env THERAPEUTIC_AREA=athena --env INDICATION=uc \
+--env DB_ANALYSIS_TABLE_SCHEMA=results --env DB_ANALYSIS_TABLE_NAME=analysis_table_uc \
+--env PIPELINE_CONFIGURATION=uc --env CONFIG_FILENAME=journey_configuration --env LOGGING=INFO \
 -v ${PWD}/results/ditran:/script/shareable_results  \
 -v disease-explorer-config:/pipeline/data \
 --network feder8-net \
